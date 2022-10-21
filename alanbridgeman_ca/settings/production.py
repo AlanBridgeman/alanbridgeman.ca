206eee7c7dfc37f20d3e168c2d930b26de55529f
Alan Bridgeman <a.bridgeman@hotmail.com>
2022-10-21 09:40:50 -0700

    Added production specific settings

--- alanbridgeman_ca/settings/production.py
+++ alanbridgeman_ca/settings/production.py
@@ -1,6 +1,24 @@
 from .base import *
 
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+
 DEBUG = False
+
+# DBHOST is only the server name, not the full URL
+hostname = os.environ['DBHOST']
+
+# Configure Postgres database; the full username for PostgreSQL flexible server is
+# username (not @sever-name).
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DBNAME'],
+        'HOST': hostname + ".postgres.database.azure.com",
+        'USER': os.environ['DBUSER'],
+        'PASSWORD': os.environ['DBPASS'] 
+    }
+}
 
 try:
     from .local import *
