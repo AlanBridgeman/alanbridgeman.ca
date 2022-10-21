b4d57ca121f9a6f6a931414b5b2aa72a04c10981
Alan Bridgeman <a.bridgeman@hotmail.com>
2022-10-21 09:53:06 -0700

    More production specific settings to try to get production/ppe working

--- alanbridgeman_ca/settings/production.py
+++ alanbridgeman_ca/settings/production.py
@@ -1,9 +1,26 @@
 from .base import *
+import os
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 DEBUG = False
+
+# WhiteNoise configuration
+MIDDLEWARE = [                                                                   
+    'django.middleware.security.SecurityMiddleware',
+# Add whitenoise middleware after the security middleware                             
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',                      
+    'django.middleware.common.CommonMiddleware',                                 
+    'django.middleware.csrf.CsrfViewMiddleware',                                 
+    'django.contrib.auth.middleware.AuthenticationMiddleware',                   
+    'django.contrib.messages.middleware.MessageMiddleware',                      
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',                    
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # DBHOST is only the server name, not the full URL
 hostname = os.environ['DBHOST']
